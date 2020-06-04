b9e441f93fcf5e68a20deff92ebd2d3b215d3bd7
nuekleer <matthewrchristensen@gmail.com>
2020-06-04 15:17:40 -0500

    cleaned up unused variable added comments

--- src/client/js/functions.js
+++ src/client/js/functions.js
@@ -1,3 +1,4 @@
+//helper for making post call
 export const postData = async ( url = '', data = {})=>{
     const response = await fetch(url, {
     method: 'POST', 
@@ -6,17 +7,19 @@ export const postData = async ( url = '', data = {})=>{
         'Content-Type': 'application/json',
     },
     body: JSON.stringify(data), // body data type must match "Content-Type" header        
-  });
+    });
 
     try {
       const newData = await response.json();
       return newData;
-    }catch(error) {
-    console.log("error", error);
-    alert("Unable to look up article at this time");
+    }
+    catch(error) {
+        console.log("error", error);
+        alert("Unable to look up article at this time");
     }
 }
 
+//update index.html
 export function updateUI(data){
     document.getElementById('polarity').innerHTML = "Polarity: " + data.polarity;
     document.getElementById('subjectivity').innerHTML = "Subjectivity: " + data.subjectivity;
--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,3 @@
-projectData = {}
-
 const dotenv = require('dotenv');
 dotenv.config();
 var path = require('path');
@@ -21,17 +19,13 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 app.use(cors());
 
-
+//api credentials
 var textapi = new aylien({
-    application_id: process.env.API_ID,
-    application_key: process.env.API_KEY
-    });
-
-
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
 
 app.use(express.static('dist'))
-
-console.log(__dirname)
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -39,33 +33,30 @@ app.get('/', function (req, res) {
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Listening on port 8081!')
+  console.log('Listening on port 8081!')
 })
 
-app.get("/showResults", function (req, res){
-  //return projectData for display in the UI
-  res.send(projectData);
-})
-
+//post route - call api
 app.post('/callNLP', function (req, res) {
-    textapi.sentiment({
-        'url': req.body.url
-      }, function(error, response) {
-        if (error === null) {
-          newEntry = {
-            polarity: response.polarity,
-            subjectivity: response.subjectivity,
-            text: response.text,
-            polarity_confidence: response.polarity_confidence,
-            subjectivity_confidence: response.subjectivity_confidence
-          }
-          projectData = newEntry;
-          res.send(newEntry);
+  textapi.sentiment({
+    'url': req.body.url
+    }, 
+    function(error, response) {
+      if (error === null) {
+        newEntry = {
+          polarity: response.polarity,
+          subjectivity: response.subjectivity,
+          text: response.text,
+          polarity_confidence: response.polarity_confidence,
+          subjectivity_confidence: response.subjectivity_confidence
         }
-        else{
-          //process error
-          console.log(error);
-          alert('Unable to check page at this time');
-        }
-      });
+        //send new entry - prevent need for extra array to be returned with a get
+        res.send(newEntry);
+      }
+      else{
+        //process error
+        console.log(error);
+        alert('Unable to check page at this time');
+      }
+  });
 })
